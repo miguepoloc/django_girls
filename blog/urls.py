3a06ba87d988884597ab9c3e553f44bb1e126245
Miguel Angel Polo Castañeda <miguepoloc@gmail.com>
2020-01-14 17:26:23 -0500

    Añadir comentarios

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
+
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
@@ -9,4 +11,10 @@ urlpatterns = [
     path('drafts/', views.post_draft_list, name='post_draft_list'),
     path('post/<pk>/publish/', views.post_publish, name='post_publish'),
     path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    url(r'^post/(?P<pk>\d+)/comment/$',
+        views.add_comment_to_post, name='add_comment_to_post'),
+    url(r'^comment/(?P<pk>\d+)/approve/$',
+        views.comment_approve, name='comment_approve'),
+    url(r'^comment/(?P<pk>\d+)/remove/$',
+        views.comment_remove, name='comment_remove'),
 ]
